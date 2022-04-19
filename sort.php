83157cf8bd5575b58ddae5f4500d0737892ecfce
Nilesh Vishwakarma <nileshv@axioned.com>
2022-04-19 13:20:00 +0530

    sort by value

--- sort.php
+++ sort.php
@@ -2,28 +2,22 @@
 
 $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
 
-$keys = array_keys($ceu);
+$values = array_values($ceu);
 
-for ($i = 0; $i < count($keys); $i++) {
+for ($i = 0; $i < count($values); $i++) {
   $j = $i;
-  while ($j > 0 && $keys[$j] < $keys[$j-1]) {
-      $tmp = $keys[$j-1];
-      $keys[$j-1] = $keys[$j];
-      $keys[$j] = $tmp;
+  while ($j > 0 && $values[$j] < $values[$j-1]) {
+      $tmp = $values[$j-1];
+      $values[$j-1] = $values[$j];
+      $values[$j] = $tmp;
       $j--;
   }
 }
 
-$sorted =  array();
-
-  foreach($keys as $key)
-  {
-    $sorted[$key] = $ceu[$key];
-  }
-
-foreach($sorted as $x => $y)
+foreach($values as $value)
 {
-  echo 'The capital of '.$x.' is '.$y.'<br>';
+  $key = array_search($value, $ceu, true);
+  echo 'The capital of '.$key.' is '.$value.'<br>';
 }
 
 ?>
